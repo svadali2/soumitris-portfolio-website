01d4781acd0e30e7b2a3b998e8061f91edf6ad73
Soumitri Vadali <svadali2@illinois.edu>
2020-09-14 20:40:46 +0800

    finished about me and contact page

--- src/components/About.js
+++ src/components/About.js
@@ -5,20 +5,19 @@ function About() {
   return (
     <div className="body">
     <h1 className="words"> About </h1>
-    <p className="words">I am a photographer based in Singapore who specializes in photographing humans, food and products.</p>
-    <p className="words">We have all been taking pictures as long as possible; I first started taking photography seriously in 2017. It began with plain street photography in Chicago, before which I moved to taking portraits.</p>
+    <p className="words">Hello, I'm Soumitri - a photographer based in Singapore who focuses on
+                          photographing humans, food and products.</p>
+    <p className="words">After studying Computer Engineering in the United States for several
+                          years, I moved back home in 2018 and recently completed my diploma in
+                          Photography and Digital Imaging at the Chatsworth Medi@rt Academy.</p>
+    <p className="words">Through an interest for film making, I bought my first DSLR in 2017. By chance,
+                          while taking street photographs, it dawned on me that I really loved taking photos.
+                          Photography gave me the base to explore the vast field of the visual arts. To
+                          incorporate compositional techniques, color theory and lighting into a single
+                          frame made me think about photography differently. It was the right blend of creative
+                          freedom and technical challenge for me to pursue.</p>
     </div>
   );
 }
 
 export default About;
-
-/*
-Hello, I'm Soumitri - a photographer based in Singapore who focuses on photographing humans, food and products.
-After studying Computer Engineering in the United States for several years, I moved back home in 2018 and
-recently completed my diploma in Photography and Digital Imaging at the Chatsworth Medi@rt Academy.
-Through an interest for film making, I bought my first DSLR in 2017. By chance, while taking street photographs,
-it dawned on me that I really loved taking photos. Photography gave me the base to explore the vast field of the
-visual arts. To incorporate compositional techniques, color theory and lighting into a single frame made me think
-about photography differently. It was the right blend of creative freedom and technical challenge for me to pursue.
-*/
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import '../styles/Contact.css';
+import emailjs from 'emailjs-com';
 
 function Contact() {
 
-  function handleSubmit(e){
+  function sendEmail(e) {
+    e.preventDefault();
 
+    emailjs.sendForm('soumitri', 'template_no343gl', e.target, 'user_ZDchW83s8BArzn46UlDbL')
+      .then((result) => {
+          console.log(result.text);
+          document.getElementById("contact-form").reset();
+      }, (error) => {
+          console.log(error.text);
+      });
   }
 
   return (
     <div>
-    <h4>Do feel free to reach out to me with any work enquiries.I am open to pro-bono and paid work at the moment. Do include important details about the project such as what is the specific job at hand, how many photographs are required and the timeline. </h4>
-    <form id="contact-form" onSubmit={handleSubmit} method="POST">
-        <div className="form-group">
-            <input placeholder="Name" className="form-control" />
-        </div>
-        <div className="form-group">
-            <input placeholder="Email Address" className="form-control" />
-        </div>
-        <div className="form-group">
-            <textarea placeholder="Message" className="text-specific" cols="40" rows="5"></textarea>
-        </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
+    <h4>Do feel free to reach out to me with any work enquiries.I am open to pro-bono and paid work at the moment. Do include specific details about the project involved and the timeline. </h4>
+    <form id="contact-form" onSubmit={sendEmail}>
+        <input placeholder="Name" name="name" className="form-control" />
+        <input placeholder="Email" name="email" className="form-control" />
+        <textarea placeholder="Message" name="message" className="text-specific" cols="40" rows="5"></textarea>
+        <button type="submit" value="Send" className="btn btn-primary">Submit</button>
     </form>
     </div>
   );
