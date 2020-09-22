bdd7c1441717cd633fa58ed25640b2201215ca4f
Soumitri Vadali <svadali2@illinois.edu>
2020-09-22 14:19:32 +0800

    fix styles

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import '../styles/About.css';
+import headshot from '../../photos/headshot.jpg';
 
 function About() {
   return (
     <div className="body">
-    <h1 className="words"> About </h1>
-    <p className="words">Hello, I'm Soumitri - a photographer based in Singapore who focuses on
-                          photographing humans, food and products.</p>
+    <h1 className="big"> Hello </h1>
+    <p className="words">
+    <img src={headshot} alt="soumitri" width="30%" align="right" height="30%" />
+    Hello, I'm Soumitri - a photographer based in Singapore who focuses on
+                          photographing humans, food and products.
+                          </p>
     <p className="words">After studying Computer Engineering in the United States for several
                           years, I moved back home in 2018 and recently completed my diploma in
                           Photography and Digital Imaging at the Chatsworth Medi@rt Academy.</p>
@@ -16,6 +20,7 @@ function About() {
                           incorporate compositional techniques, color theory and lighting into a single
                           frame made me think about photography differently. It was the right blend of creative
                           freedom and technical challenge for me to pursue.</p>
+    <p className="words">On the side, I enjoy programming, running, going to art museums and reading fiction. </p>
     </div>
   );
 }
