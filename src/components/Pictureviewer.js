d092b4fcff905ba06bbc053bf08146680c4efbe4
Soumitri Vadali <svadali2@illinois.edu>
2020-10-17 14:25:53 +0800

    everything works without thumbnails screens

--- src/components/Pictureviewer.js
+++ src/components/Pictureviewer.js
@@ -5,28 +5,58 @@ class Pictureviewer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeIndex: 0
+      activeIndex: 0,
+      viewThumbnails: false
     }
-
+    this.pictureViewer = React.createRef();
+    
   }
 
-  updateIndex = () => {
+  changeCursor = (event) => {
+    const x = event.clientX;
+    let start = this.pictureViewer.current.offsetLeft;
+    let width = this.pictureViewer.current.offsetWidth;
+    var divLeftBounds = (0.45) * width + start;
+    var divRightBounds = (0.55) * width + start;
+    console.log(x);
+    console.log(divLeftBounds);
+    console.log(divRightBounds);
+    if (x < divLeftBounds) {
+      event.target.style.cursor = 'e-resize';
+    } else if (x >= divLeftBounds && x <= divRightBounds) {
+      event.target.style.cursor = 's-resize';
+    } else if (x > divRightBounds) {
+      event.target.style.cursor = 'e-resize';
+    }
+  }
+
+  updateIndex = (event) => {
     const { activeIndex } = this.state;
     const { images } = this.props;
-    this.setState( { activeIndex: activeIndex + 1 === images.length ? 0 : activeIndex + 1 } );
+    const x = event.clientX;
+    let start = this.pictureViewer.current.offsetLeft;
+    let width = this.pictureViewer.current.offsetWidth;
+    var divLeftBounds = (0.45) * width + start;
+    var divRightBounds = (0.55) * width + start;
+    if (x < divLeftBounds) {
+      this.setState( { activeIndex: activeIndex - 1 === -1 ? images.length - 1 : activeIndex - 1 } );
+    } else if (x >= divLeftBounds && x <= divRightBounds) {
+
+    } else if (x > divRightBounds) {
+      this.setState( { activeIndex: activeIndex + 1 === images.length ? 0 : activeIndex + 1 } );
+    }
   }
 
   render() {
     const { images } = this.props;
     const { activeIndex } = this.state;
     return (
-            <div className={"container"}>
+            <div className={"container"} onClick={(e) => this.updateIndex(e)} onMouseMove={(e) => this.changeCursor(e)} ref={this.pictureViewer}>
               { 
               images.map((image, index) => {
-                return <img className={"image"+(activeIndex !== index ? " hidden" : "")} src={images[index]} alt="" onClick={this.updateIndex}/>
+                return <img key={index} className={"image"+(activeIndex !== index ? " hidden" : "")} src={images[index]} alt="" />
               })
               }
-              
             </div>
     );
   }
