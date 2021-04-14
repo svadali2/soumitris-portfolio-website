ebf31fb54a6985f1b071e089cf00d2891072e224
Soumitri Vadali <svadali2@illinois.edu>
2021-04-14 20:12:06 +0800

    Updates

---
+++ precache-manifest.293f7c2e9704d752410765ffde539521.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eab9f5dbe9fe782301f19bf1ebb044a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20e8507da7d3ac1eeb63",
+    "url": "/static/css/2.14feee9d.chunk.css"
+  },
+  {
+    "revision": "3ea5191e69063d7debd1",
+    "url": "/static/css/main.f752266b.chunk.css"
+  },
+  {
+    "revision": "20e8507da7d3ac1eeb63",
+    "url": "/static/js/2.b97ebe5b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b97ebe5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ea5191e69063d7debd1",
+    "url": "/static/js/main.99f3abd4.chunk.js"
+  },
+  {
+    "revision": "069bb8a811a35f510f8c",
+    "url": "/static/js/runtime-main.b377ca9a.js"
+  },
+  {
+    "revision": "80b9ce744a6c432fffe43823b8b75e67",
+    "url": "/static/media/bodoni-logo.80b9ce74.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "c824575a076beaa8ede22ba4d85981a2",
+    "url": "/static/media/headshot.c824575a.jpg"
+  },
+  {
+    "revision": "9e77495a8233a8d6ddc5149bf6ab1a63",
+    "url": "/static/media/portrait1.9e77495a.jpg"
+  },
+  {
+    "revision": "d3bdc0893532524fbb31fd5816fb424e",
+    "url": "/static/media/portrait10.d3bdc089.jpg"
+  },
+  {
+    "revision": "75a9b53834c9ba3dc517088f5836c314",
+    "url": "/static/media/portrait12.75a9b538.jpg"
+  },
+  {
+    "revision": "592245196e851cbb37aa99b761a82f10",
+    "url": "/static/media/portrait15.59224519.jpg"
+  },
+  {
+    "revision": "a065f0635291e13ef22eec8fdb31b3c4",
+    "url": "/static/media/portrait16.a065f063.jpg"
+  },
+  {
+    "revision": "b8dbaf85f90dd7a49b72831e4e91fa1a",
+    "url": "/static/media/portrait17.b8dbaf85.jpg"
+  },
+  {
+    "revision": "30b885fbd3a6bfc4918306211ad1b536",
+    "url": "/static/media/portrait18.30b885fb.jpg"
+  },
+  {
+    "revision": "8a6bc36ab6146c01e7d7adab60195bfe",
+    "url": "/static/media/portrait19.8a6bc36a.jpg"
+  },
+  {
+    "revision": "8361bcf3ad1fe324f6145cf783bd9001",
+    "url": "/static/media/portrait21.8361bcf3.jpg"
+  },
+  {
+    "revision": "e4612aa6318c1c7f23f707671eb3c93f",
+    "url": "/static/media/portrait22.e4612aa6.jpg"
+  },
+  {
+    "revision": "0a8031020a64b427b267021b67423191",
+    "url": "/static/media/portrait23.0a803102.jpg"
+  },
+  {
+    "revision": "792a756eb3ff018a14a33ccba37c11e2",
+    "url": "/static/media/portrait3.792a756e.jpg"
+  },
+  {
+    "revision": "5376fef0a5e2dc087f4dbb675a3128f9",
+    "url": "/static/media/portrait4.5376fef0.jpg"
+  },
+  {
+    "revision": "affc2b6f7856ed3f83532cc86d62317a",
+    "url": "/static/media/portrait5.affc2b6f.jpg"
+  },
+  {
+    "revision": "ee7f947ddf6e28c0c9fb56693079dbc2",
+    "url": "/static/media/portrait8.ee7f947d.jpg"
+  },
+  {
+    "revision": "4961c5dc14a8eb3f89e4602341b76235",
+    "url": "/static/media/portrait9.4961c5dc.jpg"
+  },
+  {
+    "revision": "757ec72c1f9f9a5581589f5b9b4da464",
+    "url": "/static/media/product1.757ec72c.jpg"
+  },
+  {
+    "revision": "deb403693c4c5bd06f8b2b68590cd168",
+    "url": "/static/media/product2.deb40369.jpg"
+  },
+  {
+    "revision": "323afe2fea871831da5e59fb85c6c146",
+    "url": "/static/media/product4.323afe2f.jpg"
+  },
+  {
+    "revision": "2ecb275d036f0a701d09c4d093b84710",
+    "url": "/static/media/product5.2ecb275d.jpg"
+  },
+  {
+    "revision": "1a62d90661e69fdfe3dcd7bd3c5c497c",
+    "url": "/static/media/product6.1a62d906.jpg"
+  },
+  {
+    "revision": "e99fccf90f76b2b8d90ec9bd57db4464",
+    "url": "/static/media/product7.e99fccf9.jpg"
+  }
+]);
